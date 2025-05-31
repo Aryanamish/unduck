d382a3195c374ebde8761d1a5ea043e868e2e520
Aryan Amish <34622095+Aryanamish@users.noreply.github.com>
2025-06-01 01:01:50 +0530

    fixed ts war ning

--- src/main.ts
+++ src/main.ts
@@ -89,22 +89,7 @@ function doBangRedirect(query: string) {
   window.location.href = searchUrl;
 }
 
-function registerServiceWorker() {
-  if ("serviceWorker" in navigator) {
-    // Register a service worker hosted at the root of the
-    // site using the default scope.
-    navigator.serviceWorker.register("/src/sw.js").then(
-      (registration) => {
-        console.log("Service worker registration succeeded:", registration);
-      },
-      (error) => {
-        console.error(`Service worker registration failed: ${error}`);
-      },
-    );
-  } else {
-    console.error("Service workers are not supported.");
-  }
-}
+
 
 function doRedirect() {
   const url = new URL(window.location.href);
